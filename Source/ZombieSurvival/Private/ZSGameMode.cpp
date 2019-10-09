215cbdaf93b686c721aa51bb64324e25c27660e5
rohitrtk <rohit_rtk@hotmail.com>
2019-10-09 01:22:37 -0400

    Bug with spawning zombies

---
+++ Source/ZombieSurvival/Private/ZSGameMode.cpp
@@ -0,0 +1,2 @@
+#include "ZSGameMode.h"
+
---
+++ Source/ZombieSurvival/Public/ZSGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZSGameMode.generated.h"
+
+UCLASS()
+class ZOMBIESURVIVAL_API AZSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
